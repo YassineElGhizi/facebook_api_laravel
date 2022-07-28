63e2afdbf4e1bf794c826c58ebbcb7c02ef690ad
yassineelghizi <yassine.ghizi2@gmail.com>
2022-07-28 19:45:26 +0100

    mailsv2

--- resources/views/weeklyMails.blade.php
+++ resources/views/weeklyMails.blade.php
@@ -1,5 +1,5 @@
-Hey there!
-This is your weekly digest from {{$app_name}}.
-We shared {{$number}} social media posts on your accounts this week.
-Thanks for using {{$app_name}}!
-Cheers,
+Hey there!<br>
+This is your weekly digest from {{$app_name}}.<br>
+We shared {{$number}} social media posts on your accounts this week.<br>
+Thanks for using {{$app_name}}!<br>
+Cheers,<br>
